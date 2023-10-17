9aef74351d0853565b54531eeea1aa74a79c2902
Fabio Tea <iam@f4b.io>
2023-10-17 18:20:01 +0200

    removed typescript

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import "feather-icons";
 import Typewriter from "typewriter-effect/dist/core";
-import { hideSplashscreen } from "./util";
+import { hideSplashscreen } from "./util.js";
 
 setTimeout(() => {
   hideSplashscreen();
@@ -9,10 +9,10 @@ setTimeout(() => {
 
 const typewriterElement = document.getElementById("typewriter");
 const outputResultElement = document.getElementById("output-result");
-if(typewriterElement) {
+if (typewriterElement) {
   const typewriter = new Typewriter(typewriterElement, {
     loop: false,
-    delay: "natural"
+    delay: "natural",
   });
   typewriter
     .pasteString("<span class='text-slate-100'>[</span>")
@@ -27,5 +27,5 @@ if(typewriterElement) {
       outputResultElement?.classList.remove("hidden");
       typewriter.stop();
     });
-    typewriter.start();
+  typewriter.start();
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,ts}",
-    "./index.html"
-  ],
+  content: ["./src/**/*.{html,js,ts}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
