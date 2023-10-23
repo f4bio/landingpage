3b4f912a7467f3f4485f770ea6976985c1dc0b94
Fabio Tea <iam@f4b.io>
2023-10-23 12:45:54 +0200

    removed typescript

--- src/main.js
+++ src/main.js
@@ -3,22 +3,25 @@ import "feather-icons";
 import Typed from "typed.js";
 import { hideSplashscreen } from "./util.js";
 
+function startTyping() {
+  if (document.getElementById("typed")) {
+    const outputResultElement = document.getElementById("output-result");
+
+    const typed = new Typed("#typed", {
+      strings: ["whoami | tree ^1000\n"],
+      typeSpeed: 100,
+      startDelay: 1000,
+      onComplete: () => {
+        // typed.stop();
+        console.log("completed!", typed);
+        outputResultElement.classList.remove("hidden");
+      },
+    });
+  }
+}
+
 setTimeout(() => {
   hideSplashscreen();
-}, 0);
 
-if (document.getElementById("typed")) {
-  const outputResultElement = document.getElementById("output-result");
-
-  const typed = new Typed("#typed", {
-    strings: ["whoami | tree ^1000\n"],
-    typeSpeed: 100,
-    startDelay: 1000,
-    onComplete: () => {
-      console.log("completed!");
-      typed.stop();
-      outputResultElement.classList.remove("hidden");
-    },
-  });
-  console.log(typed);
-}
+  startTyping();
+}, 2000);
