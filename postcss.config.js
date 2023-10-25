77a043fa786e0eb218a2d4c37182ca2f7e164f32
Fabio Tea <iam@f4b.io>
2023-10-26 01:33:37 +0200

    replaced purgecss with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,12 @@
 import autoprefixer from "autoprefixer";
 import postcssImport from "postcss-import";
 import tailwindcss from "tailwindcss";
+import cssnano from "cssnano";
 import tailwindcssNesting from "./node_modules/tailwindcss/nesting/index.js";
-import purgecss from "@fullhuman/postcss-purgecss";
 
 /** @type {import("postcss-load-config").Config} */
 const config = {
-  plugins: [
-    autoprefixer,
-    postcssImport,
-    tailwindcssNesting,
-    tailwindcss,
-    purgecss({
-      content: ["./src/index.html"],
-    }),
-  ],
+  plugins: [autoprefixer, postcssImport, tailwindcssNesting, tailwindcss, cssnano],
 };
 
 export default config;
